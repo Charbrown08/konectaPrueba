36d9a42ff81cb0ffdc55d02d876871ca896c60dc
Charbrown08 <cartam15@hotmail.com>
2023-01-05 23:54:15 -0500

    Ingresando Base de datos Konecta

---
+++ src/main/java/com/prueba/konecta/PruebakonectaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.konecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebakonectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebakonectaApplication.class, args);
+	}
+
+}
